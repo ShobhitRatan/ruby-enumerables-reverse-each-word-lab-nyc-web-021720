49207b2e9ce660aa78c895b903a9c7ef648d6eb6
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-06 03:09:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,4 +5,12 @@ def reverse_each_word(string)
     words.reverse!
   end
   array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split
+  array.collect do |words|
+    words.reverse!
+  end
+  array.join(" ")
 end 
